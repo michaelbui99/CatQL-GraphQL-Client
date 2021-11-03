378a55a47303e3cf0d45820feca8897e3df26308
Michael Bui <michaelbui99@hotmail.com>
2021-11-03 23:08:25 +0100

    Added new prop to GqlQuery and Required annotation

--- src/CatQLClient/Client/GqlQuery.cs
+++ src/CatQLClient/Client/GqlQuery.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CatQL.GraphQL.Client
 {
     public class GqlQuery
     {
-    
+        [Required]
         public string Query { get; set; }
+        public string OperationName { get; set; }
+        
+        
         public string Variables { get; set; }
     }
 }
