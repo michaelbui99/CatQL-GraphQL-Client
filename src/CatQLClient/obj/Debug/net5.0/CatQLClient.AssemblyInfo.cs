3e6a5aec05a906d496ef0c9991531950ee5020d1
Michael Bui <michaelbui99@hotmail.com>
2021-11-03 23:17:17 +0100

    Added package metadata

--- src/CatQLClient/obj/Debug/net5.0/CatQLClient.AssemblyInfo.cs
+++ src/CatQLClient/obj/Debug/net5.0/CatQLClient.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CatQLClient")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Michael Bui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Simple GraphQL Client for making Queries and Mutations to a GraphQL endpoint")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
